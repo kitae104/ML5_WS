ab8ffbd4cd20474074dfe8143d505645a60313ba
Kitae's Github <aqua0405@gmail.com>
2023-01-29 03:20:05 +0900

    ejs 레이아웃 처리

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,29 +1,55 @@
-// 동물, 사물 분류하기
-let img, classifier;
+// 웹 캠 신체 부위별 포인트 찾기
+let img1;
+let size=0;
+let video;
+let poseNet;
+let poses=[];
+let keypointX=[];
+let keypointY=[];
+let skeletons=[];
 
 function preload(){
-  img = loadImage('images/머그컵.jpg');
-  classifier = ml5.imageClassifier('MobileNet', modelReady);
+  //img1 = loadImage('/images/soccer3.jpg');  
 }
 
 function setup() {
-  createCanvas(img.width, img.height+30);
-  background(0);
-  image(img, 0, 0);
+  var cnv = createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide()
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on('pose', gotResult);
+  cnv.position(1100, 250);
 }
 
-function modelReady(){
-  console.log('MobileNet 모델 불러오기 완료');
-  classifier.classify(img, gotResult);
+function modelReady(){  
+  console.log('model OK');
 }
 
-function gotResult(err, results){
-  if(err){
-    console.error(err);
-    return;
+function gotResult(results){
+  poses = results[0].pose.keypoints;
+  skeletons = results[0].skeleton;
+}
+
+function draw(){
+  imageMode(CORNER);
+  image(video, 0, 0);
+
+  fill('#FFFF00');
+  stroke('#FF0000');
+  strokeWeight(3);
+  for(let i = 0; i < poses.length; i++){
+    keypointX[i]=round(poses[i].position.x);
+    keypointY[i]=round(poses[i].position.y);
+    ellipse(keypointX[i], keypointY[i],10);
   }
-  
-  fill(255, 255, 0);
-  textSize(24);
-  text(results[0].label, 2, height-5);
-}          
+  stroke('#FFFF00');
+  strokeWeight(3);
+  for(let i=0; i< skeletons.length; i++){
+    line(round(skeletons[i][0].position.x), round(skeletons[i][0].position.y), round(skeletons[i][1].position.x), round(skeletons[i][1].position.y));
+  }
+  size = round(dist(keypointX[0],keypointY[0],keypointX[3],keypointY[3]));
+  imageMode(CENTER);
+    
+  fill(255, 255, 0, 200);
+  ellipse(keypointX[0], keypointY[0]-20, 80, 100);  
+}
---
+++ server2.js
@@ -0,0 +1,68 @@
+'use strict';
+let code;
+const express = require('express');
+const path = require('path');
+const {spawn} = require('child_process');
+const bodyParser = require("body-parser");
+const fs = require('fs').promises;
+const expressLayouts = require('express-ejs-layouts');
+
+
+// const cors = require('cors');
+
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// 레이아웃 설정 
+app.use(expressLayouts);
+
+app.set('views', __dirname +'/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.set('layout','./layouts/main');
+app.set('layout extractScripts', true);
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+  res.render('index', {title: 'EJS 테스트'});
+});
+
+app.get('/test', function (req, res) {
+  res.render('test', {title: '레이아웃 테스트', layout: './layouts/sidebar'});
+});
+ 
+
+app.post('/run', (req, res) => {	 
+	code = req.body.program;
+	
+  var html = 
+	  '<!DOCTYPE html>\n' +
+		'<html lang="en">\n' +
+		'	<head>\n' +
+		'		<title>ML5_D3 체험 페이지</title>\n' +
+		'		<meta name="viewport" content="width=device-width, initial-scale=1.0">\n' +
+		
+		'		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js"></script>\n' +
+		'		<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/addons/p5.sound.min.js"></script>\n' +		
+		'		<script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>\n' +
+		//'		<script src="https://d3js.org/d3.v7.min.js"></script>\n' +	
+		'		<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.16.0/d3.js"></script>\n' +	
+		'	</head>\n' +
+		
+		'	<body>\n' +
+		'		<h2>ML5_D3 체험 페이지</h2>\n' +
+		
+		'		<script>\n' + code +
+				
+		'\n		</script>\n' +
+
+		'	</body>\n' +
+		'</html>'
+	
+	res.send(html);
+});
+
+app.listen(3000, function() {});
